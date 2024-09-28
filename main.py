da8adf1bd092162d01b5a93fa352df4d049aeab5
abtin <saeediab@sheridancollege.ca>
2024-09-28 18:14:50 -0400

    First docstring and layed out steps

---
+++ main.py
@@ -0,0 +1,21 @@
+"""Naruto: Shinobi Quest Version 1
+This is the initial main program where
+program will be prepared before splitting into modules"""
+
+#imports
+
+#create dictionaries
+
+#define functions
+
+#1. Print a welcome message to the user and provide a short overview of the game including it's name and goal
+
+#2. Give user choice of character
+
+#3. Present quest challenges one at a time
+#Present challenges
+#Allow user to role dice
+#Calculate challenge results
+#Inform user of results
+
+#4. After all challenges, display win or loss message to user
